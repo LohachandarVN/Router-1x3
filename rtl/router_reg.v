17c3bc1a0b248b395fa0c2a4a13d2b6dc484c575
LOHACHANDAR V N <159149097+LohachandarVN@users.noreply.github.com>
2025-08-30 15:27:27 +0530

    Update router_reg.v

--- rtl/router_reg.v
+++ rtl/router_reg.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Lohachandar_V_N
 // 
 // Create Date: 22.08.2025 13:25:33
 // Design Name: 
